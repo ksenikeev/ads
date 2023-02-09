b2f6326cee394a2a0f56d74f005c0059506eac1e
Kamil <ksenikeev@mail.ru>
2023-02-09 13:51:38 +0300

    Lab1

--- lab1/src/main/java/ru/itis/aisd/lab1/Main.java
+++ lab1/src/main/java/ru/itis/aisd/lab1/Main.java
@@ -3,11 +3,56 @@ package ru.itis.aisd.lab1;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.Stack;
 
 public class Main {
     public static void main(String[] args) {
         String str = read("transport.json");
         System.out.println(str);
+
+        Stack<Character> stack = new Stack<>();
+
+        for (int i = 0; i < str.length(); ++i) {
+            switch (str.charAt(i)) {
+                case '{':
+                    if (!stack.isEmpty() && stack.peek() == '"')
+                        break;
+                    stack.push(str.charAt(i));
+                    break;
+                case '[':
+                    if (!stack.isEmpty() && stack.peek() == '"')
+                        break;
+                    stack.push(str.charAt(i));
+                    break;
+                case '"':
+                    if (!stack.isEmpty() && stack.peek() == '"') {
+                        stack.pop();
+                    } else {
+                        stack.push(str.charAt(i));
+                    }
+                    break;
+                case '}':
+                    if (!stack.isEmpty() && stack.peek() == '"')
+                        break;
+                    if (!stack.isEmpty() && stack.peek() == '{') {
+                        stack.pop();
+                    } else {
+                        System.out.println("Problem in " + i);
+                        return;
+                    }
+                    break;
+                case ']':
+                    if (!stack.isEmpty() && stack.peek() == '"')
+                        break;
+                    if (!stack.isEmpty() && stack.peek() == '[') {
+                        stack.pop();
+                    } else {
+                        System.out.println("Problem in " + i);
+                        return;
+                    }
+                    break;
+            }
+        }
     }
 
     public static String read(String fileName) {
